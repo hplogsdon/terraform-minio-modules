162c4fbd8a757c831ff61910fab3c1f412380965
uxp <uxp@hplogsdon.com>
2025-07-24 20:46:15 -0600

    chore: formatting

--- modules/minio-bucket/main.tf
+++ modules/minio-bucket/main.tf
@@ -1,12 +1,12 @@
 
 resource "minio_s3_bucket" "this" {
-  bucket = var.name
+  bucket         = var.name
   object_locking = var.object_locking
 }
 
 resource "minio_s3_bucket_versioning" "this" {
   bucket = minio_s3_bucket.this.id
-  count = var.object_versioning ? 1 : 0
+  count  = var.object_versioning ? 1 : 0
 
   versioning_configuration {
     status = "Enabled"
