594013baebc1cb9f63a47776825fbc4a5e9ac482
uxp <uxp@hplogsdon.com>
2025-07-24 20:42:09 -0600

    feat: add versioning and locking

--- modules/minio-bucket/main.tf
+++ modules/minio-bucket/main.tf
@@ -1,4 +1,14 @@
 
-resource "minio_s3_bucket" "s3_bucket" {
+resource "minio_s3_bucket" "this" {
   bucket = var.name
+  object_locking = var.object_locking
+}
+
+resource "minio_s3_bucket_versioning" "this" {
+  bucket = minio_s3_bucket.this.id
+  count = var.object_versioning ? 1 : 0
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
